4801952271b1875466d094d0faf93ed05956945b
mohamedalisalaim <fury45409@gmail.com>
2024-11-29 11:05:34 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:poetry_gate/pages/intro_page.dart";
+import "package:poetry_gate/pages/home_page.dart";
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: IntroPage(),
+      home: HomePage(),
     );
   }
 }
